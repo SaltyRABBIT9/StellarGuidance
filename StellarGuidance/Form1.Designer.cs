fc8aeb4d4d2b2675bfc1824503750e0e2e896ddb
SaltyRABBIT9 <89496271+SaltyRABBIT9@users.noreply.github.com>
2023-10-03 22:12:38 +0600

    Add files via upload

---
+++ StellarGuidance/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace StellarGuidance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Submit = new System.Windows.Forms.Button();
+            this.date = new System.Windows.Forms.TextBox();
+            this.month = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Clear = new System.Windows.Forms.Button();
+            this.image = new System.Windows.Forms.Panel();
+            this.des = new System.Windows.Forms.Label();
+            this.zodiac = new System.Windows.Forms.ImageList(this.components);
+            this.zod = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(201, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 71);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "StellarGuidance";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(186, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(370, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Find everything about your Zodic";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(141, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ENTER YOUR BIRTHDAY";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(361, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 28);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(463, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 28);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Month";
+            // 
+            // Submit
+            // 
+            this.Submit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.Submit.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Submit.ForeColor = System.Drawing.Color.Black;
+            this.Submit.Location = new System.Drawing.Point(470, 226);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(75, 30);
+            this.Submit.TabIndex = 6;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = false;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // date
+            // 
+            this.date.Location = new System.Drawing.Point(352, 178);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(70, 20);
+            this.date.TabIndex = 8;
+            // 
+            // month
+            // 
+            this.month.Location = new System.Drawing.Point(448, 179);
+            this.month.Name = "month";
+            this.month.Size = new System.Drawing.Size(93, 20);
+            this.month.TabIndex = 9;
+            this.month.TextChanged += new System.EventHandler(this.month_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(428, 178);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 24);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "/";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.Clear.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clear.ForeColor = System.Drawing.Color.Black;
+            this.Clear.Location = new System.Drawing.Point(389, 226);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 30);
+            this.Clear.TabIndex = 11;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = false;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // image
+            // 
+            this.image.BackColor = System.Drawing.Color.Transparent;
+            this.image.ForeColor = System.Drawing.Color.Transparent;
+            this.image.Location = new System.Drawing.Point(134, 279);
+            this.image.Name = "image";
+            this.image.Size = new System.Drawing.Size(212, 234);
+            this.image.TabIndex = 12;
+            this.image.Paint += new System.Windows.Forms.PaintEventHandler(this.image_Paint);
+            // 
+            // des
+            // 
+            this.des.BackColor = System.Drawing.Color.Transparent;
+            this.des.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.des.ForeColor = System.Drawing.Color.White;
+            this.des.Location = new System.Drawing.Point(353, 279);
+            this.des.Name = "des";
+            this.des.Size = new System.Drawing.Size(235, 234);
+            this.des.TabIndex = 13;
+            // 
+            // zodiac
+            // 
+            this.zodiac.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("zodiac.ImageStream")));
+            this.zodiac.TransparentColor = System.Drawing.Color.Transparent;
+            this.zodiac.Images.SetKeyName(0, "0.jpg");
+            this.zodiac.Images.SetKeyName(1, "1.jpg");
+            this.zodiac.Images.SetKeyName(2, "2.jpg");
+            this.zodiac.Images.SetKeyName(3, "3.jpg");
+            this.zodiac.Images.SetKeyName(4, "4.jpg");
+            this.zodiac.Images.SetKeyName(5, "5.jpg");
+            this.zodiac.Images.SetKeyName(6, "6.jpg");
+            this.zodiac.Images.SetKeyName(7, "7.jpg");
+            this.zodiac.Images.SetKeyName(8, "8.jpg");
+            this.zodiac.Images.SetKeyName(9, "9.jpg");
+            this.zodiac.Images.SetKeyName(10, "10.jpg");
+            this.zodiac.Images.SetKeyName(11, "11.jpg");
+            // 
+            // zod
+            // 
+            this.zod.AutoSize = true;
+            this.zod.BackColor = System.Drawing.Color.Transparent;
+            this.zod.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.zod.ForeColor = System.Drawing.Color.White;
+            this.zod.Location = new System.Drawing.Point(168, 234);
+            this.zod.Name = "zod";
+            this.zod.Size = new System.Drawing.Size(0, 43);
+            this.zod.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(727, 589);
+            this.Controls.Add(this.zod);
+            this.Controls.Add(this.des);
+            this.Controls.Add(this.image);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.month);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "StellarGuidance";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.TextBox date;
+        private System.Windows.Forms.TextBox month;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Panel image;
+        private System.Windows.Forms.Label des;
+        private System.Windows.Forms.ImageList zodiac;
+        private System.Windows.Forms.Label zod;
+    }
+}
+
---
+++ StellarGuidance/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace StellarGuidance
+{
+    public enum zodiacsigns
+    {
+        Taurs, Aries, Libra, Leo, Gemini, Pieces, Capricorn, Sagittarius, Virgo, Cancer, Aquaries, Scorpio
+    }
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+
+
+                if (month.Text.All(char.IsDigit))
+                {
+                    MessageBox.Show("You forgot to specify which month you were born in.");
+                }
+                else if (date.Text.All(char.IsDigit))
+                {
+                }
+
+                if (double.Parse(date.Text) > 31)
+                {
+                    MessageBox.Show("Please Enter a Valid Date");
+                }
+                else
+                {
+                    if (month.Text == "december")
+                    {
+                        if (double.Parse(date.Text) < 22)
+                        {
+                            zod.Text= " " + zodiacsigns.Capricorn;
+                            des.Text = "Capricorn individuals are known for their discipline, ambition, and practicality. They are often responsible, reliable, and have a strong desire for success.";
+                            image.BackgroundImage = zodiac.Images[0];
+                        }
+                        else
+                        {
+
+                            zod.Text = " " + zodiacsigns.Sagittarius;
+                            des.Text = "Sagittarius individuals are known for their adventurous spirit, optimism, and love for freedom. They are often open-minded, philosophical, and enjoy exploring new horizons.";
+                            image.BackgroundImage = zodiac.Images[11];
+                        }
+                    }
+
+
+
+                    if (month.Text == "january")
+                    {
+                        if (double.Parse(date.Text) < 20)
+                        {
+                            zod.Text = " " + zodiacsigns.Capricorn;
+                            des.Text = "Capricorn individuals are known for their discipline, ambition, and practicality. They are often responsible, reliable, and have a strong desire for success.";
+                            image.BackgroundImage = zodiac.Images[0];
+                        }
+                        else
+                        {
+                            zod.Text = " " + zodiacsigns.Aquaries;
+                            des.Text = "Aquarius individuals are known for their independence, intellectualism, and humanitarian nature. They are often visionary, progressive, and value equality.";
+                            image.BackgroundImage = zodiac.Images[1];
+
+                        }
+                    }
+
+                    if (month.Text == "feburary")
+                    {
+                        if (double.Parse(date.Text) < 19)
+                        {
+                            zod.Text = " " + zodiacsigns.Aquaries;
+                            des.Text = "Aquarius individuals are known for their independence, intellectualism, and humanitarian nature. They are often visionary, progressive, and value equality.";
+                            image.BackgroundImage = zodiac.Images[1];
+                        }
+                        else
+                        {
+
+                            zod.Text = " " + zodiacsigns.Pieces;
+                            des.Text = "Pisces individuals are known for their empathy, creativity, and dreamy nature. They are often compassionate, intuitive, and have a deep connection to their emotions.";
+                            image.BackgroundImage = zodiac.Images[2];
+                        }
+                    }
+
+                    if (month.Text == "march")
+                    {
+                        if (double.Parse(date.Text) < 21)
+                        {
+
+                            zod.Text = " " + zodiacsigns.Pieces;
+                            des.Text = "Pisces individuals are known for their empathy, creativity, and dreamy nature. They are often compassionate, intuitive, and have a deep connection to their emotions.";
+                            image.BackgroundImage = zodiac.Images[2];
+                        }
+                        else
+                        {
+
+                            zod.Text = " " + zodiacsigns.Aries;
+                            des.Text = "Aries individuals are known for their confidence, courage, and assertiveness. They are natural leaders, energetic, and often have a competitive streak.";
+                            image.BackgroundImage = zodiac.Images[3];
+                        }
+                    }
+                    if (month.Text == "april")
+                    {
+                        if (double.Parse(date.Text) < 20)
+                        {
+
+                            zod.Text = " " + zodiacsigns.Aries;
+                            des.Text = "Aries individuals are known for their confidence, courage, and assertiveness. They are natural leaders, energetic, and often have a competitive streak.";
+                            image.BackgroundImage = zodiac.Images[3];
+                        }
+                        else
+                        {
+
+                            zod.Text = " " + zodiacsigns.Taurs;
+                            des.Text = "Taurus individuals are known for their stability, patience, and practicality. They are often reliable, determined, and have a strong sense of loyalty.";
+                            image.BackgroundImage = zodiac.Images[4];
+                        }
+                    }
+                    if (month.Text == "may")
+                    {
+                        if (double.Parse(date.Text) < 21)
+                        {
+
+                            zod.Text = " " + zodiacsigns.Taurs;
+                            des.Text = "Taurus individuals are known for their stability,\n patience, and practicality. They are often reliable, determined, and have a strong sense of loyalty.";
+                            image.BackgroundImage = zodiac.Images[4];
+                        }
+                        else
+                        {
+
+                            zod.Text = " " + zodiacsigns.Gemini;
+                            des.Text = "Gemini individuals are known for their versatility, adaptability, and wit. They are often curious, expressive, and enjoy intellectual conversations.";
+                            image.BackgroundImage = zodiac.Images[5];
+                        }
+                    }
+                    if (month.Text == "june")
+                    {
+                        if (double.Parse(date.Text) < 21)
+                        {
+
+                            zod.Text = " " + zodiacsigns.Gemini;
+                            des.Text = "Gemini individuals are known for their versatility, adaptability, and wit. They are often curious, expressive, and enjoy intellectual conversations.";
+                            image.BackgroundImage = zodiac.Images[5];
+                        }
+                        else
+                        {
+
+                            zod.Text = " " + zodiacsigns.Cancer;
+                            des.Text = "Cancer individuals are known for their sensitivity, empathy, and nurturing nature. They are often deeply intuitive, family-oriented, and value emotional connections.";
+                            image.BackgroundImage = zodiac.Images[6];
+
+                        }
+                    }
+                    if (month.Text == "july")
+                    {
+                        if (double.Parse(date.Text) < 22)
+                        {
+
+                            zod.Text = " " + zodiacsigns.Cancer;
+                            des.Text = "Cancer individuals are known for their sensitivity, empathy, and nurturing nature. They are often deeply intuitive, family-oriented, and value emotional connections.";
+                            image.BackgroundImage = zodiac.Images[6];
+                        }
+                        else
+                        {
+
+                            zod.Text = " " + zodiacsigns.Leo;
+                            des.Text = "Leo individuals are known for their confidence, charisma, and leadership qualities. They are often generous, ambitious, and enjoy being the center of attention.";
+                            image.BackgroundImage = zodiac.Images[7];
+                        }
+                    }
+                    if (month.Text == "august")
+                    {
+                        if (double.Parse(date.Text) < 23)
+                        {
+
+                            zod.Text = " " + zodiacsigns.Leo;
+                            des.Text = "Leo individuals are known for their confidence, charisma, and leadership qualities. They are often generous, ambitious, and enjoy being the center of attention.";
+                            image.BackgroundImage = zodiac.Images[7];
+                        }
+                        else
+                        {
+
+                            zod.Text = " " + zodiacsigns.Virgo;
+                            des.Text = "Virgo individuals are known for their practicality, attention to detail, and analytical nature. They are often hardworking, reliable, and have a strong sense of responsibility.";
+                            image.BackgroundImage = zodiac.Images[8];
+                        }
+                    }
+                    if (month.Text == "september")
+                    {
+                        if (double.Parse(date.Text) < 23)
+                        {
+
+                            zod.Text = " " + zodiacsigns.Virgo;
+                            des.Text = "Virgo individuals are known for their practicality, attention to detail, and analytical nature. They are often hardworking, reliable, and have a strong sense of responsibility.";
+                            image.BackgroundImage = zodiac.Images[8];
+                        }
+                        else
+                        {
+
+                            zod.Text = " " + zodiacsigns.Libra;
+                            des.Text = "Libra individuals are known for their diplomacy, harmony-seeking nature, and love for beauty. They are often social, fair-minded, and value peaceful relationships.";
+                            image.BackgroundImage = zodiac.Images[9];
+
+                        }
+                    }
+                    if (month.Text == "october")
+                    {
+                        if (double.Parse(date.Text) < 23)
+                        {
+
+                            zod.Text = " " + zodiacsigns.Libra;
+                            des.Text = "Libra individuals are known for their diplomacy, harmony-seeking nature, and love for beauty. They are often social, fair-minded, and value peaceful relationships.";
+                            image.BackgroundImage = zodiac.Images[9];
+                        }
+                        else
+                        {
+
+                            zod.Text = " " + zodiacsigns.Scorpio;
+                            des.Text = "Scorpio individuals are known for their intensity, passion, and deep emotional nature. They are often resourceful, determined, and have a keen ability to uncover secrets.";
+                            image.BackgroundImage = zodiac.Images[10];
+                        }
+                    }
+                    if (month.Text == "november")
+                    {
+                        if (double.Parse(date.Text) < 22)
+                        {
+
+                            zod.Text = " " + zodiacsigns.Scorpio;
+                            des.Text = "Scorpio individuals are known for their intensity, passion, and deep emotional nature. They are often resourceful, determined, and have a keen ability to uncover secrets.";
+                            image.BackgroundImage = zodiac.Images[10];
+                        }
+                        else
+                        {
+
+                            zod.Text = " " + zodiacsigns.Sagittarius;
+                            des.Text = "Sagittarius individuals are known for their adventurous spirit, optimism, and love for freedom. They are often open-minded, philosophical, and enjoy exploring new horizons.";
+                            image.BackgroundImage = zodiac.Images[11];
+                        }
+                    }
+
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("please enter number for the day");
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        //private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+       // {
+
+       // }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void month_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            date.ResetText();
+            month.ResetText();
+            des.ResetText();
+            zod.ResetText();
+
+            image.BackgroundImage = null;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void image_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
